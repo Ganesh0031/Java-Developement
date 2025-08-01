3dce82813e7a905cad57ba1b09a22c37dd6e7cb0
Ganesh0031 <gingle0731@gmail.com>
2025-08-01 23:44:24 +0530

    Third

---
+++ IncDec1.java
@@ -0,0 +1,15 @@
+public class IncDec1{
+public static void main(String []args){
+
+//int a=5;
+//System.out.println(a);
+//a++;
+//++a;
+//System.out.println(a);
+int a=5;
+int b;
+b=a++ +--a+a---a++;
+System.out.println(a);
+System.out.println(b);
+
+}}
